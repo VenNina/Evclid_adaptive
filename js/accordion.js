0f498a12ab3a713db68b97598c66ed5c3cda2b5a
VenNina <76688955+VenNina@users.noreply.github.com>
2021-05-13 12:49:31 +0300

    Add files via upload

---
+++ js/accordion.js
@@ -0,0 +1,5 @@
+// "usestrech";
+
+$(function () {
+  $("#accordion").accordion();
+});
---
+++ js/click.js
@@ -0,0 +1,7 @@
+// "usestrech";
+
+window.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("#burger").addEventListener("click", function () {
+    document.querySelector("#nav-menu").classList.toggle("nav-burger_is-active")
+  })
+})
